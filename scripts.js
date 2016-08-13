ca9e35f542b3f0df30115b3fa70adc88a58364cf
Elizabeth Sanger <libsang@gmail.com>
2016-08-13 14:00:23 -0500

    base constructor ready

--- scripts.js
+++ scripts.js
@@ -1,1 +1,33 @@
 console.log('scripts connected')
+
+$(document).ready(function (){
+
+$('#output').html(`<div>Output working, bitches</div>`)
+
+var Battledrome = (function(battledrome){
+
+battledrome.Feline = function() {
+
+    this.color = null;
+    this.health = Math.floor(Math.random() * 10 + 40);
+    this.damage = Math.floor(Math.random() * 6 + 10);
+    this.attitude = null;
+
+};
+
+battledrome.Feline.prototype.setColor = function (newDescription) {
+    this.description = newDescription
+}
+
+
+battledrome.Feline.prototype.setAttitude = function(newAttitude) {
+  this.attitude = newAttitude;
+}
+
+return battledrome
+
+})
+
+(Battledrome || {})
+
+})
